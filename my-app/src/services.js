1f90d03508ba3567597a2f8c1568b452816d4d3b
guoclara <114608292+upsettiger123@users.noreply.github.com>
2022-12-02 13:17:50 -0500

    fix async calls

--- my-app/src/services.js
+++ my-app/src/services.js
@@ -7,35 +7,52 @@ export const services = {
   getOneWord
 }
 
-function getAllWords() {
-  axios.get(`${API_ROOT}/allWords`)
-    .then((dictionary) => {
-      return dictionary.data.map((word) => ({
-        word: word.word,
-        meaning: word.meaning,
-        context: word.context,
-        stigma: word.stigma,
-        substitutions: word.substitutions,
-      }))
-    }).catch((error) => {
-      console.log("getAllWords ERROR: ", error);
-      return error
-    })
+async function getAllWords() {
+  try {
+    const response = await axios.get(`${API_ROOT}/allWords`);
+    const dictionary = response.data
+    console.log(dictionary)
+    return dictionary.map((word) => ({
+            word: word.word,
+            meaning: word.meaning,
+            context: word.context,
+            stigma: word.stigma,
+            substitutions: word.substitutions,
+          }))
+  } catch (error) {
+    console.log("error", error);
+    return null;
+  }
+  
+  // axios.get(`${API_ROOT}/allWords`)
+  //   .then((dictionary) => {
+  //     return dictionary.data.map((word) => ({
+  //       word: word.word,
+  //       meaning: word.meaning,
+  //       context: word.context,
+  //       stigma: word.stigma,
+  //       substitutions: word.substitutions,
+  //     }))
+  //   }).catch((error) => {
+  //     console.log("getAllWords ERROR: ", error);
+  //     return error
+  //   })
 }
 
-function getOneWord(word) {
-  axios.get(`${API_ROOT}/oneWord?word=` + word)
-    .then((word) => {
-      const wordData = word.data
-      return {
-        word: wordData.word,
-        meaning: wordData.meaning,
-        context: wordData.context,
-        stigma: wordData.stigma,
-        substitutions: wordData.substitutions,
-      }
-    }).catch((error) => {
-      console.log("getOneWord ERROR", error);
-      return error
-    })
+async function getOneWord(word) {
+  try {
+    const response = await axios.get(`${API_ROOT}/oneWord?word=` + word);
+    const wordData = response.data
+    return {
+            word: wordData.word,
+            meaning: wordData.meaning,
+            context: wordData.context,
+            stigma: wordData.stigma,
+            substitutions: wordData.substitutions,
+          }
+  } catch (error) {
+    console.log("error", error);
+    return null;
+  }
+  
 }
