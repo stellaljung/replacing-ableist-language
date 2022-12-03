6fec930120d0608c2ba0f779f0e7feef55a029b7
Sophie Ljung <sophie_ljung@brown.edu>
2022-12-02 19:28:42 -0500

    small css changes

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
     <div className="App" >
       <header className="App" aria-label="Website Information">
         <h1 className="App-header" aria-label="Website Title">Replacing Ableist Language</h1>
-        <h1 className="App-description" aria-label="Website Description">Welcome to our dictionary for disability justice! Type “dictionary” into the command box to see all the words, their meanings and substitutions in our dictionary. Type any word into the command box to see its meaning, context in the field of disability studies, how it contributes to disability stigma, and some of our substitutions. Contact stella_ljung@brown.edu with any word submissions of your own!</h1>
+        <div className="App-description" aria-label="Website Description">Welcome to our dictionary for disability justice! Type “dictionary” into the command box to see all the words, their meanings and substitutions in our dictionary. Type any word into the command box to see its meaning, context in the field of disability studies, how it contributes to disability stigma, and some of our substitutions. Contact stella_ljung@brown.edu with any word submissions of your own!</div>
       </header>
       <SearchBox />
     </div>
